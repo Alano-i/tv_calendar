1ba1ad9534420915a9b90e75832a9d79e1ee049c
Alano-i <68833595+Alano-i@users.noreply.github.com>
2023-01-06 16:32:40 +0800

    增加自定义推送消息封面以及增加运行细节日志输出

--- event.py
+++ event.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
 import datetime
+import random
 import typing
 from typing import Dict, Any
 
@@ -11,10 +13,9 @@ from mbot.openapi import mbot_api
 from mbot.core.plugins import *
 
 import logging
+import time
 import yaml
 import re
-
-import os
 
 server = mbot_api
 api_url = "/3/tv/%(tv_id)s/season/%(season_number)s"
@@ -26,23 +27,27 @@ message_to_uid: typing.List[int] = []
 
 @plugin.after_setup
 def after_setup(plugin_meta: PluginMeta, config: Dict[str, Any]):
+    global set_pic_url
     global message_to_uid
+    set_pic_url = config.get('set_pic_url')
     message_to_uid = config.get('uid')
 
-    if os.path.exists('/app/frontend/static/tv_calendar.html'):
-        os.remove('/app/frontend/static/tv_calendar.html')
-    if os.path.exists('/app/frontend/static/episode.html'):
-        os.remove('/app/frontend/static/episode.html')
-    if os.path.exists('/app/frontend/static/banner.jpg'):
-        os.remove('/app/frontend/static/banner.jpg')
+    # if os.path.exists('/app/frontend/static/tv_calendar.html'):
+    #     os.remove('/app/frontend/static/tv_calendar.html')
+    # if os.path.exists('/app/frontend/static/episode.html'):
+    #     os.remove('/app/frontend/static/episode.html')
     shutil.copy('/data/plugins/tv_calendar/frontend/tv_calendar.html', '/app/frontend/static')
     shutil.copy('/data/plugins/tv_calendar/frontend/episode.html', '/app/frontend/static')
-    shutil.copy('/data/plugins/tv_calendar/frontend/banner.jpg', '/app/frontend/static')
-
+    shutil.copy('/data/plugins/tv_calendar/frontend/ALIBABA-Font-Bold.otf', '/app/frontend/static')
+    shutil.copy('/data/plugins/tv_calendar/frontend/bg.png', '/app/frontend/static')
+    shutil.copy('/data/plugins/tv_calendar/frontend/title.png', '/app/frontend/static')
+    _LOGGER.info(f'追剧日历插件: WEB 页素材已复制并覆盖到「/app/frontend/static」')
 
 @plugin.config_changed
 def config_changed(config: Dict[str, Any]):
+    global set_pic_url
     global message_to_uid
+    set_pic_url = config.get('set_pic_url')
     message_to_uid = config.get('uid')
 
 
@@ -50,28 +55,40 @@ def config_changed(config: Dict[str, Any]):
 def task():
     save_json()
 
-
 def get_tmdb_info(tv_id, season_number):
+    result = ''
     for i in range(5):
         try:
-            res = server.tmdb.request_api(api_url % {'tv_id': tv_id, 'season_number': season_number}, param)
+            result = server.tmdb.request_api(api_url % {'tv_id': tv_id, 'season_number': season_number}, param)
+            break
         except Exception as e:
-            _LOGGER.error(e)
+            _LOGGER.error(f'「get_tmdb_info」 请求异常，原因：{e}')
+            time.sleep(5)
             continue
-        return res
+    if result:
+        _LOGGER.info(f'「get_tmdb_info」请求成功')
+        return result
+    else:
+        _LOGGER.error('「get_tmdb_info」请求获取失败，可能还没有这个剧集的信息')
     return False
-
 
 def get_tv_info(tv_id):
+    result = ''
     for i in range(5):
         try:
-            res = server.tmdb.request_api(tv_api_url % {'tv_id': tv_id}, param)
+            result = server.tmdb.request_api(tv_api_url % {'tv_id': tv_id}, param)
+            # _LOGGER.error(f'「get_tv_info」请求结果：{result}')
+            break
         except Exception as e:
-            _LOGGER.error(e)
+            _LOGGER.error(f'「get_tv_info」请求异常，原因：{e}')
+            time.sleep(5)
             continue
-        return res
-    return False
-
+    if result:
+        _LOGGER.info(f'「get_tv_info」请求成功')
+        return result
+    else:
+        _LOGGER.error('「get_tv_info」请求获取失败，可能还没有这个剧集的信息')
+        return False
 
 def find_season_poster(seasons, season_number):
     for season in seasons:
@@ -95,6 +112,7 @@ def save_json():
         tv_poster = tv['poster_path']
         seasons = tv['seasons']
         tv_name = tv['name']
+        _LOGGER.info(f'开始处理「{tv_name}」')
         tv_original_name = tv['original_name']
         season_poster = find_season_poster(seasons, row.season_number)
         season = get_tmdb_info(row.tmdb_id, row.season_number)
@@ -107,17 +125,21 @@ def save_json():
             episode['tv_poster'] = tv_poster
             episode['season_poster'] = season_poster
             episode_arr.append(episode)
-
-    with open('/app/frontend/static/original.json', 'w', encoding='utf-8') as fp:
-        json.dump(episode_arr, fp, ensure_ascii=False)
-
+    try:
+        with open('/app/frontend/static/original.json', 'w', encoding='utf-8') as fp:
+            json.dump(episode_arr, fp, ensure_ascii=False)
+        _LOGGER.info('开始写入新的追剧日历数据到「original.json」文件')
+    except Exception as e:
+            _LOGGER.error(f'写入新「original.json」文件出错，原因: {e}')
+            pass
     _LOGGER.info('剧集数据更新结束')
+    _LOGGER.info('开始推送今日将要更新的剧集信息')
     push_message()
+    _LOGGER.info('追剧日历数据更新进程全部完成')
 
 
 def get_after_day(day, n):
     # 今天的时间
-
     # 计算偏移量
     offset = datetime.timedelta(days=n)
     # 获取想要的日期的时间
@@ -126,64 +148,89 @@ def get_after_day(day, n):
 
 
 def get_server_url():
-    yml_file = "/data/conf/base_config.yml"
-    with open(yml_file, encoding='utf-8') as f:
-        yml_data = yaml.load(f, Loader=yaml.FullLoader)
-    mr_url = yml_data['web']['server_url']
-    if (re.match('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', mr_url) != None):
-        return mr_url
-    return False
-
-
+    try:
+        yml_file = "/data/conf/base_config.yml"
+        with open(yml_file, encoding='utf-8') as f:
+            yml_data = yaml.load(f, Loader=yaml.FullLoader)
+        mr_url = yml_data['web']['server_url']
+        if (re.match('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', mr_url) != None):
+            _LOGGER.info(f'从配置文件中获取到的「mr_url」{mr_url}')
+            return mr_url
+    except Exception as e:
+                _LOGGER.error(f'获取「mr_url」异常，原因: {e}')
+                pass
+    return '' 
 def push_message():
-    _LOGGER.info('推送今日更新')
-    with open('/app/frontend/static/original.json', encoding='utf-8') as f:
-        episode_arr = json.load(f)
-    episode_filter = list(
-        filter(lambda x: x['air_date'] == datetime.date.today().strftime('%Y-%m-%d'), episode_arr))
-    name_dict = {}
-    for item in episode_filter:
-        if item['tv_name'] not in name_dict:
-            name_dict[item['tv_name']] = [item]
-        else:
-            name_dict[item['tv_name']].append(item)
-    img_api = 'https://cdn-us.imgs.moe/2023/01/06/63b7930dc9331.jpg'
-    count = 0
-    if len(episode_arr) == 0:
-        message = "今日没有剧集更新"
-    else:
-        message_arr = []
-        for tv_name in name_dict:
-            count = count + 1
-            episodes = name_dict[tv_name]
-            episode_number_arr = []
-            for episode in episodes:
-                episode_number_arr.append(str(episode['episode_number']))
-            episode_numbers = ','.join(episode_number_arr)
-            message_arr.append(
-                tv_name + 'S' + str(episodes[0]['season_number']) + 'E' + episode_numbers)
-        message = "\n".join(message_arr)
-
-    mr_url = get_server_url()
+    mr_url = ''
     link_url = ''
-    if (mr_url != False):
-        if (mr_url[-1] != '/'):
-            mr_url += '/'
-        link_url = mr_url + 'static/tv_calendar.html'
-    if message_to_uid:
-        for _ in message_to_uid:
-            server.notify.send_message_by_tmpl('{{title}}', '{{a}}', {
-                'title': '今日剧集更新 共' + str(count) + '部',
-                'a': message,
-                'link_url': link_url,
-                'pic_url': img_api
-            }, to_uid=_)
-    else:
-        server.notify.send_message_by_tmpl('{{title}}', '{{a}}', {
-            'title': '今日剧集更新 共' + str(count) + '部',
-            'a': message,
-            'link_url': link_url,
-            'pic_url': img_api
+    msg_title = ''
+    pic_url = ''
+    mr_url = get_server_url()
+    if mr_url:
+        link_url = f'{mr_url}/static/tv_calendar.html'
+        # _LOGGER.error(f'mr_url: {mr_url}，all:{link_url}')
+    try:
+        with open('/app/frontend/static/original.json', encoding='utf-8') as f:
+            episode_arr = json.load(f)
+        episode_filter = list(
+            filter(lambda x: x['air_date'] == datetime.date.today().strftime('%Y-%m-%d'), episode_arr))
+        name_dict = {}
+        for item in episode_filter:
+            if item['tv_name'] not in name_dict:
+                name_dict[item['tv_name']] = [item]
+            else:
+                name_dict[item['tv_name']].append(item)
+        img_api = 'https://api.r10086.com/img-api.php?type=%E5%8A%A8%E6%BC%AB%E7%BB%BC%E5%90%88' + str(
+            random.randint(1, 18))
+        
+        _LOGGER.info(f'设置的消息封面图片地址: {set_pic_url}')
+        if set_pic_url:
+            pic_url = set_pic_url
+        else:
+            pic_url = img_api
 
-        })
-    _LOGGER.info('完成推送')
+        if len(episode_arr) == 0:
+            message = "今日没有剧集更新"
+        else:
+            message_arr = []
+            tv_count = len(name_dict)
+            if tv_count:
+                msg_title = f'今天将更新 {int(tv_count):02d} 部剧集'
+            else:
+                msg_title = "今日没有剧集更新"
+            for tv_name in name_dict:
+                episodes = name_dict[tv_name]
+                episode_number_arr = []
+                for episode in episodes:
+                    episode_number_arr.append(str(episode['episode_number']))
+                episode_numbers = ','.join([f"{int(episode):02d}" for episode in episode_number_arr])
+                message_arr.append(f"{tv_name}  第 {episodes[0]['season_number']:02d} 季·第 {episode_numbers} 集")
+            message = "\n".join(message_arr)
+    except Exception as e:
+        _LOGGER.error(f'处理异常，原因: {e}')
+        pass
+    try:
+        if message_to_uid:
+            for _ in message_to_uid:
+                try:
+                    server.notify.send_message_by_tmpl('{{title}}', '{{a}}', {
+                        'title': msg_title,
+                        'a': message,
+                        'pic_url': pic_url,
+                        'link_url': link_url
+                    }, to_uid=_)
+                    _LOGGER.info(f'「今日剧集更新列表」已推送通知')
+                except Exception as e:
+                    _LOGGER.error(f'消息推送异常，原因: {e}')
+                    pass
+        else:
+            server.notify.send_message_by_tmpl('{{title}}', '{{a}}', {
+                'title': msg_title,
+                'a': message,
+                'pic_url': pic_url,
+                'link_url': link_url
+            })
+            _LOGGER.info(f'「今日剧集更新列表」已推送通知')
+    except Exception as e:
+                    _LOGGER.error(f'消息推送异常，原因: {e}')
+                    pass
