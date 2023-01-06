9ff48db8d0b92bf7ef5f43fffbc47f5b2fc8a82d
Alano-i <68833595+Alano-i@users.noreply.github.com>
2023-01-06 16:31:10 +0800

    Update command.py

--- command.py
+++ command.py
@@ -8,14 +8,14 @@ server = mbot_api
 _LOGGER = logging.getLogger(__name__)
 
 
-@plugin.command(name='echo', title='初始化追剧日历数据', desc='订阅剧集越多，执行时间越长，请耐心等待', icon='AlarmOn', run_in_background=True)
+@plugin.command(name='echo', title='更新追剧日历数据', desc='订阅剧集越多，执行时间越长，请耐心等待', icon='CalendarMonthOutlined', run_in_background=True)
 def echo(ctx: PluginCommandContext):
     """
-    异步执行,签到测试
+    异步执行,更新追剧日历数据
     """
     try:
         save_json()
     except Exception as e:
         _LOGGER.error(e)
-        return PluginCommandResponse(True, f'创建数据源失败')
-    return PluginCommandResponse(True, f'创建数据源成功')
+        return PluginCommandResponse(True, f'创建追剧日历数据源失败')
+    return PluginCommandResponse(True, f'创建追剧日历数据源成功')
